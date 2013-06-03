a449dc55af4b82ccf185d6da9bad559c2a97541e
Oliver Hallam <oliver.hallam@gmail.com>
2013-06-03 23:29:31 +0100

    Added support for appfog

--- app/app.js
+++ app/app.js
@@ -15,7 +15,7 @@ var squabbleServer = require('./squabble-server')
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.VCAP_APP_PORT || process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
